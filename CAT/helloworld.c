bd869b408cf319226649124f8b792c331c0049d8
Miika Pynttäri <miika.pynttari@student.lut.fi>
2021-04-19 02:23:07 -0700

    Added new folders for each of the subprojects

---
+++ CAT/helloworld.c
@@ -0,0 +1,12 @@
+/* AUTHOR (of this very complicated program): Miika Pynttäri */
+
+#include <stdio.h>
+
+
+int main() {
+   
+   /* this is a comment */
+   
+   printf("Hello, World!");
+   return 0;
+}
