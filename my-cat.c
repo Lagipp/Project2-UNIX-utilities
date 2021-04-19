52f710da3356678bf14a9ece057baa7d687cc784
Miika Pynttäri <miika.pynttari@student.lut.fi>
2021-04-19 02:06:51 -0700

    First commit

---
+++ my-cat.c
@@ -0,0 +1,24 @@
+/*****************************************************************/
+/* KÄYTTÖJÄRJESTELMÄT JA SYSTEEMIOHJELMOINTI
+ * Project 2:			UNIX utilities
+ * Nimi ja opiskelijanro:	Miika Pynttäri, 0563090
+ * Päivämäärä:			19.4.2021
+ * Yhteistyö ja lähteet:	<>
+*/
+/*****************************************************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 512
+
+
+int main(void)
+{
+
+	
+
+	fprintf(stdout, "== DEBUG: end of program ==\n\n");
+	return 0;
+}
