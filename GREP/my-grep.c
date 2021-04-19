c69594e4f4f67e6df8b2982daab8ad14901224c5
Miika Pynttäri <miika.pynttari@student.lut.fi>
2021-04-19 04:54:11 -0700

    First commit

---
+++ GREP/my-grep.c
@@ -0,0 +1,22 @@
+/*****************************************************************/
+/* KÄYTTÖJÄRJESTELMÄT JA SYSTEEMIOHJELMOINTI
+ * Project 2:			UNIX utilities
+ * Nimi ja opiskelijanro:	Miika Pynttäri, 0563090
+ * Päivämäärä:			19.4.2021
+ * Yhteistyö ja lähteet:	 
+*/
+/*****************************************************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main(int argc, char* argv[])
+{
+	
+	fprintf(stdout, "-- DEBUG: end of program --\n");
+	exit(0);
+}
