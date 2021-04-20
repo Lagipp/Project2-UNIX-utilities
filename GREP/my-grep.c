c4dd9f7fa7a7e2b8a74a8e98e7a6b113c958ddb2
Miika Pynttäri <miika.pynttari@student.lut.fi>
2021-04-20 06:06:12 -0700

    did some cleanup and finished the program

--- GREP/my-grep.c
+++ GREP/my-grep.c
@@ -2,8 +2,8 @@
 /* KÄYTTÖJÄRJESTELMÄT JA SYSTEEMIOHJELMOINTI
  * Project 2:			UNIX utilities
  * Nimi ja opiskelijanro:	Miika Pynttäri, 0563090
- * Päivämäärä:			19.4.2021
- * Yhteistyö ja lähteet:	https://www.codeproject.com/Questions/5268549/How-do-I-make-a-programme-in-C-which-mimic-the-gre
+ * Päivämäärä:			20.4.2021
+ * Yhteistyö ja lähteet:	
 */
 /*****************************************************************/
 
@@ -24,35 +24,89 @@ int main(int argc, char* argv[])
 	char filename[MAX];
 	char searchterm[MAX];
 	
+	
 	if (argc < 2)
 	{
+	/* when only the command is typed? */ 
+	
+	
 		fprintf(stdout, "INCORRECT USAGE, try: ./my-grep <searchterm> <file1>, <file2>, ...\n");
 		exit(1);
 	}
 	
+	
 	if (argc == 2)
 	{
-		// searching from stdin
-		printf("\n == DEBUG: tää ei viel toimi lol ota joku toinen ==\n");
+	/* when no files are given, just the searchterm --> take words from user (read from stdin) */
+	
+	
+		/* user input into a char-array found from: 
+		   https://stackoverflow.com/questions/17504122/taking-a-string-as-input-and-storing-them-in-a-character-array-in-c/17504265 */
+	
+	
+		char *line = NULL;
+		size_t len;
+		char array[MAX][MAX];		/* https://stackoverflow.com/questions/7251307/using-strcpy-with-a-string-array-in-c */
+						/* for some reason the array needs double brackets, idk why but it works */
+						/* (i had some problems but they were solved with the link above) */
+		int u;
+		
+		strcpy(searchterm, argv[1]);
+		
+		
+		fprintf(stdout, "Give some inputs, type 'STOP' to stop\n\n");
+		
+		count = 0;						/* resetting the value of count */
+		
+		while(getline(&line, &len, stdin) != -1)
+		{
+			if (strcmp(line, "STOP\n") == 0)		/* if user types STOP, don't take any more inputs */
+			{
+				break;
+			}
+		
+			if (strstr(line, searchterm) != NULL)		/* if user typed something containing the searchterm, */
+			{					   	/* add it to the array */
+				strcpy(array[m], line);
+				count++;				/* keeping count how many words were found */
+			}
+			m++;
+		}
+		
+		free(line);
+		
+		
+		/* PRINTING OUT THE FOUND WORDS */
+		
+		fprintf(stdout, "--- Printing out words with '%s' in them from stdin:\n", searchterm);
+		
+		for(u = 0; u < m; u++)
+		{
+			fprintf(stdout, "%s", array[u]);
+		}
+		
+		printf("--- Found %d occurrences!\n\n", count);
 		return 0;
 	}
 	
 	
 	if (argc > 2)
 	{
+	/* when the searchterm and file(s) have been supplied */
+	
 	
 	/* expanded from the following source: https://www.codeproject.com/Questions/5268549/How-do-I-make-a-programme-in-C-which-mimic-the-gre */
 	
 	
-		for (m = 2; m < argc; m++)
+		for (m = 2; m < argc; m++)		/* looping through until all files are printed */
 		{
 			count = 0;				/* resetting the value of count */
 			
-			strcpy(filename, argv[m]);		/* saving the parameters into variables for easier print messages */
+			strcpy(filename, argv[m]);
 			strcpy(searchterm, argv[1]);
 			
 			file = fopen(argv[m], "r");
-			if (file == NULL)
+			if (file == NULL)			/* if the file doesn't exist (or something odd happens for some reason), stop */
 			{
 				fprintf(stdout, "error: cannot open file '%s'\n", filename);
 				exit(1);
@@ -68,17 +122,13 @@ int main(int argc, char* argv[])
 				if (strstr(buffer, argv[1]))		/* if the searchterm is found within a word, the word gets printed */
 				{
 					printf("%s", buffer);
-					count++;
+					count++;			/* keeping count how many words were found */
 				}
 			}
 		
 			fclose(file);
-			printf("--- Found %d occurrences!\n\n", count);
-			
+			printf("--- Found %d occurrences!\n\n", count);	
 		}
-		
 	}
-	
-	fprintf(stdout, "-- DEBUG: end of program --\n");
 	return 0;
 }
