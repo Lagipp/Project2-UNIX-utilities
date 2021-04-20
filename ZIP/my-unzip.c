64ebc66f1762e2f496973c2a2e8bca13ffaf3213
Miika Pynttäri <miika.pynttari@student.lut.fi>
2021-04-20 11:33:51 -0700

    first commit

---
+++ ZIP/my-unzip.c
@@ -0,0 +1,23 @@
+/*****************************************************************/
+/* KÄYTTÖJÄRJESTELMÄT JA SYSTEEMIOHJELMOINTI
+ * Project 2:			UNIX utilities
+ * Nimi ja opiskelijanro:	Miika Pynttäri, 0563090
+ * Päivämäärä:			20.4.2021
+ * Yhteistyö ja lähteet:	
+*/
+/*****************************************************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 256
+
+
+int main(int argc, char* argv[])
+{
+
+
+	fprintf(stdout, "== DEBUG: end of program\n\n");
+	return 0;
+}
